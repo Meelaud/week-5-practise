633b114f4ffb675cab86e71dc4ca7cfa4e76b7ca
Meelaud <meelaudtotonchi@gmail.com>
2023-02-05 12:34:38 -0800

    first

---
+++ playwright.config.js
@@ -0,0 +1,51 @@
+const { devices } = reqiure('@playwright/test')
+
+/** @type {import('@playwright/test'.PlaywrightTestConfig) } */
+const config = {
+    //Option shared for all projects.
+    timeout: 30000,
+    use: {
+        ignoreHTTPSErrors: true
+    },
+    //Options specific to each project browser size
+    projects: [
+        {
+            name: 'Desktop Chromium',
+            use: {
+                browserName: 'chromium',
+                viewport: { width: 1280, height: 720 },
+            }
+        },
+        {
+            name: 'Desktop Safari',
+            use: {
+                browserName: 'webkit',
+                viewport: { width: 1280, height: 720 },
+            },
+        },
+        {
+            name: 'Desktop Firefox',
+            use: {
+                browserName: 'firefox',
+                viewport: { width: 1280, height: 720 },
+            }
+        },
+        {
+            name: 'Mobile Chrome',
+            use: {
+                browserName: 'chromium',
+                ...devices['iphone XR'],
+                viewport: {width: 414, height: 896},
+            }
+        },
+        {
+            name: 'Tablet Chrome',
+            use: {
+                browserName: 'chromium',
+                ...devices ['ipad air'],
+                viewport: {width: 820, height: 1180},
+            }
+        },
+        
+    ]
+}
